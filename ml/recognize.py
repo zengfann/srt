592512dac22fe608a2251e3601ad61d715d85d9c
毛亚琛 <maoyachen55@gmail.com>
2021-05-25 19:09:41 +0800

    修复sourcechangewarning

--- ml/recognize.py
+++ ml/recognize.py
@@ -1,14 +1,18 @@
+import warnings
 from os import getenv
 
 import numpy as np
 import torch
+from torch.serialization import SourceChangeWarning
 from torchvision import transforms
+
+warnings.filterwarnings("ignore", category=SourceChangeWarning)
 
 IMG_HEIGHT = 224
 IMG_WIDTH = 224
 NUM_CHANNELS = 3
-norm_mean = [0.485, 0.456, 0.406]
-norm_std = [0.229, 0.224, 0.225]
+NORM_MEAN = [0.485, 0.456, 0.406]
+NORM_STD = [0.229, 0.224, 0.225]
 
 MODEL_PATH = getenv("MODEL_PATH")
 
@@ -16,7 +20,7 @@ valid_transform = transforms.Compose(
     [
         transforms.Resize((IMG_HEIGHT, IMG_WIDTH)),
         transforms.ToTensor(),
-        transforms.Normalize(norm_mean, norm_std),
+        transforms.Normalize(NORM_MEAN, NORM_STD),
     ]
 )
 
@@ -34,9 +38,7 @@ def recognize(im):
     np_im = np.array([np_im])
     x = torch.tensor(np_im)
     x = x.to(device)
-    # return 1
     with torch.no_grad():
         out = model(x)
         prediction1 = torch.max(out, 1)[1]
-        print(prediction1.item())
         return prediction1.item()
